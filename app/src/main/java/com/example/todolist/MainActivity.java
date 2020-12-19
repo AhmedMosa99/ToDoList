ff44151b7b1ddd633a72f8adf2ed13d81a030ee9
unknown <houda.mosa.2020@gmail.com>
2020-12-20 01:39:06 +0200

    commit third

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.todolist.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+    TextView BtnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BtnNext=findViewById(R.id.BtnNext);
+        BtnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, activity_login.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
